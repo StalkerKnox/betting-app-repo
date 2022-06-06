fa85b5f8a9cd2745c5766e8445b34a12cbcde958
StalkerKnox <milisicdino@gmail.com>
2022-06-06 10:41:33 +0200

    define structs and parse JSON

--- main.go
+++ main.go
@@ -9,75 +9,85 @@ import (
 	"time"
 )
 
-type Liga struct {
-	Naziv   string `json:"naziv"`
-	Razrade []struct {
-		Tipovi []struct {
-			Naziv string `json:"naziv"`
-		} `json:"tipovi"`
-		Ponude []int `json:"ponude"`
-	} `json:"razrade"`
+// Creating GetLeagueResponse Struct
+
+type GetLeagueResponse struct {
+	League []League `json:"lige"`
 }
 
-type Ponuda struct {
-	Broj     string    `json:"broj"`
-	ID       int       `json:"id"`
-	Naziv    string    `json:"naziv"`
-	Vrijeme  time.Time `json:"vrijeme"`
-	Tecajevi []struct {
-		Tecaj float64 `json:"tecaj"`
-		Naziv string  `json:"naziv"`
-	} `json:"tecajevi"`
-	TvKanal       string `json:"tv_kanal,omitempty"`
-	ImaStatistiku bool   `json:"ima_statistiku,omitempty"`
+type League struct {
+	Title       string        `json:"naziv"`
+	Elaboration []Elaboration `json:"razrade"`
+}
+
+type Elaboration struct {
+	Type []Type `json:"tipovi"`
+	Name []int  `json:"ponude"`
+}
+
+type Type struct {
+	Name string `json:"Naziv"`
+}
+
+// Creating GetOfferResponse Struct
+
+type GetOfferResponse struct {
+	Number        string    `json:"broj"`
+	TVchannel     string    `json:"tv_kanal"`
+	ID            int       `json:"id"`
+	Title         string    `json:"name"`
+	HasStatistics bool      `json:"ima_statistiku"`
+	Time          time.Time `json:"vrijeme"`
+	Rate          []Rate    `json:"tecajevi"`
+}
+
+type Rate struct {
+	Rates float64 `json:"tecaj"`
+	Name  string  `json:"naziv"`
 }
 
 func main() {
-	// Dohvacanje prvog JSON-a
+	// Getting First JSON
 
-	resp, err := http.Get("https://minus5-dev-test.s3.eu-central-1.amazonaws.com/lige.jso")
+	resp, err := http.Get("https://minus5-dev-test.s3.eu-central-1.amazonaws.com/lige.json")
 	if err != nil {
-		fmt.Println(err)
-
+		log.Fatal(err)
 	}
 
-	bodyLige, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	leaugesBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
-	var lige []Liga
-	jsonErrLige := json.Unmarshal(bodyLige, &lige)
-	if jsonErrLige != nil {
-		log.Println("Error unmarshalling json data:", err)
+	var leagues GetLeagueResponse
+	jsonErrLeagues := json.Unmarshal(leaugesBody, &leagues)
+	if jsonErrLeagues != nil {
+		log.Fatal("Error unmarshalling json data:", err)
 	}
-	fmt.Println(lige)
+	fmt.Println(leagues)
 
-	// ligeJSON := string(bodyPonude)
-	// fmt.Println(ligeJSON)
+	fmt.Println("#######################################################################################################")
 
-	fmt.Print("#######################################################################################################")
-
-	//Dohvacanje drugog JSON-a
+	//Getting second JSON
 
 	res, err := http.Get("https://minus5-dev-test.s3.eu-central-1.amazonaws.com/ponude.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	bodyPonude, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	offersBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	var ponude []Ponuda
-	jsonErrPonude := json.Unmarshal(bodyPonude, &ponude)
+	var offers []GetOfferResponse
+	jsonErrPonude := json.Unmarshal(offersBody, &offers)
 	if jsonErrPonude != nil {
 		log.Fatal(jsonErrPonude)
 	}
-	fmt.Print(ponude)
-
-	// ponudeJSON := string(bodyPonude)
-	// fmt.Println(ponudeJSON)
-
+	fmt.Print(offers)
 }
